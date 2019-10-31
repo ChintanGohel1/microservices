1cade5de5d2a643effc4226a9f1fa1ff33752802
ChintanGohel1 <chintan.gohel@ics-global.in>
2019-10-31 10:49:55 +0530

    initial commit

---
+++ spring-cloud-config-server/src/test/java/com/chintangohel/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chintangohel.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
