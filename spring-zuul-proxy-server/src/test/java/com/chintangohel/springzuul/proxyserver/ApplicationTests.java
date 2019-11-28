be5f33e01d268c0c911b08c9e282fecb80a3f53c
ChintanGohel1 <chintan.gohel@ics-global.in>
2019-11-28 09:31:33 +0530

    initial commit

---
+++ spring-zuul-proxy-server/src/test/java/com/chintangohel/springzuul/proxyserver/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chintangohel.springzuul.proxyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
